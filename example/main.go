f04a13a6da9ae732257c2720340602876eaec902
Lucas Toledo <toledo_lu@gmail.com>
2023-07-11 16:02:08 -0300

    Change project structure

--- example/main.go
+++ example/main.go
@@ -2,13 +2,7 @@ package main
 
 import (
 	gbp "github.com/LucasToledoPereira/go-boilerplate"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/auth"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/collection"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/game"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/migration"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/studio"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/template"
-	"github.com/LucasToledoPereira/go-boilerplate/internal/modules/user"
+	gameapi "github.com/LucasToledoPereira/go-boilerplate/apis/game"
 )
 
 // @title Go Boilerplate API Example
@@ -18,12 +12,7 @@ import (
 // @BasePath /api/v1
 func main() {
 	builder := gbp.Default()
-	builder.Register(migration.MigrationModuleFunc(), auth.AuthModuleFunc())
-	builder.Register(studio.StudioModuleFunc())
-	builder.Register(user.UserModuleFunc())
-	builder.Register(game.GameModuleFunc())
-	builder.Register(template.TemplateModuleFunc())
-	builder.Register(collection.CollectionModuleFunc())
+	gameapi.LoadGameAPI(builder)
 	server, _ := builder.Bootstrap()
 	server.Run()
 }
